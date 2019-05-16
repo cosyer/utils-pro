7e1808778669ef191d5c640d363044ede6e98833
chenyu <535509852@qq.com>
2019-05-17 00:19:48 +0800

    device

--- src/device/getExplore.js
+++ src/device/getExplore.js
@@ -1,27 +1,87 @@
 /**
- * 
+ *
  * @desc 获取浏览器类型和版本
- * @return {String} 
+ * @return {String}
  */
 function getExplore() {
-    var sys = {},
-        ua = navigator.userAgent.toLowerCase(),
-        s;
-    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
-        (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
-        (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
-        (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
-        (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
-        (s = ua.match(/chrome\/([\d\.]+)/)) ? sys.chrome = s[1] :
-        (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
-    // 根据关系进行判断
-    if (sys.ie) return ('IE: ' + sys.ie)
-    if (sys.edge) return ('EDGE: ' + sys.edge)
-    if (sys.firefox) return ('Firefox: ' + sys.firefox)
-    if (sys.chrome) return ('Chrome: ' + sys.chrome)
-    if (sys.opera) return ('Opera: ' + sys.opera)
-    if (sys.safari) return ('Safari: ' + sys.safari)
-    return 'Unkonwn'
+  var sys = {},
+    ua = navigator.userAgent.toLowerCase(),
+    s;
+  (s = ua.match(/rv:([\d.]+)\) like gecko/))
+    ? (sys.ie = s[1])
+    : (s = ua.match(/msie ([\d\.]+)/))
+    ? (sys.ie = s[1])
+    : (s = ua.match(/edge\/([\d\.]+)/))
+    ? (sys.edge = s[1])
+    : (s = ua.match(/firefox\/([\d\.]+)/))
+    ? (sys.firefox = s[1])
+    : (s = ua.match(/(?:opera|opr).([\d\.]+)/))
+    ? (sys.opera = s[1])
+    : (s = ua.match(/chrome\/([\d\.]+)/))
+    ? (sys.chrome = s[1])
+    : (s = ua.match(/version\/([\d\.]+).*safari/))
+    ? (sys.safari = s[1])
+    : 0;
+  // 根据关系进行判断
+  if (sys.ie) return "IE: " + sys.ie;
+  if (sys.edge) return "EDGE: " + sys.edge;
+  if (sys.firefox) return "Firefox: " + sys.firefox;
+  if (sys.chrome) return "Chrome: " + sys.chrome;
+  if (sys.opera) return "Opera: " + sys.opera;
+  if (sys.safari) return "Safari: " + sys.safari;
+  return "Unknown";
+}
+
+// 判断当前浏览类型
+function BrowserType() {
+  var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+  var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
+  var isIE =
+    userAgent.indexOf("compatible") > -1 &&
+    userAgent.indexOf("MSIE") > -1 &&
+    !isOpera; //判断是否IE浏览器
+  var isEdge = userAgent.indexOf("Windows NT 6.1; Trident/7.0;") > -1 && !isIE; //判断是否IE的Edge浏览器
+  var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器
+  var isSafari =
+    userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1; //判断是否Safari浏览器
+  var isChrome =
+    userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1; //判断Chrome浏览器
+
+  if (isIE) {
+    var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+    reIE.test(userAgent);
+    var fIEVersion = parseFloat(RegExp["$1"]);
+    if (fIEVersion == 7) {
+      return "IE7";
+    } else if (fIEVersion == 8) {
+      return "IE8";
+    } else if (fIEVersion == 9) {
+      return "IE9";
+    } else if (fIEVersion == 10) {
+      return "IE10";
+    } else if (fIEVersion == 11) {
+      return "IE11";
+    } else {
+      return "0";
+    } //IE版本过低
+  } //isIE end
+
+  if (isFF) {
+    return "FF";
+  }
+  if (isOpera) {
+    return "Opera";
+  }
+  if (isSafari) {
+    return "Safari";
+  }
+  if (isChrome) {
+    return "Chrome";
+  }
+  if (isEdge) {
+    return "Edge";
+  }
+  return "Unknown";
 }
 
 module.exports = getExplore;
